d14b6c7ec402018c01278032fc9a581ecf495349
Anukrishnanbiju <akbbiju08@gmail.com>
2026-08-29 00:08:13 +0530

    Time: 141 ms (50.76%) | Memory: 37.5 MB (30.74%) - LeetSync

--- 220-contains-duplicate-iii/contains-duplicate-iii.py
+++ 220-contains-duplicate-iii/contains-duplicate-iii.py
@@ -12,21 +12,17 @@ class Solution:
         for i, x in enumerate(nums):
             b = get_bucket(x)
 
-            # Same bucket
             if b in buckets:
                 return True
 
-            # Left bucket
             if b - 1 in buckets and abs(x - buckets[b - 1]) <= valueDiff:
                 return True
 
-            # Right bucket
             if b + 1 in buckets and abs(x - buckets[b + 1]) <= valueDiff:
                 return True
 
             buckets[b] = x
 
-            # Keep only indexDiff previous elements
             if i >= indexDiff:
                 old = nums[i - indexDiff]
                 del buckets[get_bucket(old)]
