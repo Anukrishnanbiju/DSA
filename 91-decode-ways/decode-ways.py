975c58696a7a3b6ee8e7fd06a86d716d8ec589c6
Anukrishnanbiju <akbbiju08@gmail.com>
2026-09-21 18:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.82%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numDecodings(self, s):
+        a, b = 1, 0
+
+        for i in range(len(s) - 1, -1, -1):
+            cur = 0
+
+            if s[i] != '0':
+                cur = a
+
+                if i + 1 < len(s) and 10 <= int(s[i:i+2]) <= 26:
+                    cur += b
+
+            b = a
+            a = cur
+
+        return a
