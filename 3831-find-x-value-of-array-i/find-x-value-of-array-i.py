30f40992e278c7107c62cb3fdfeedc1ef07c2a18
Anukrishnanbiju <akbbiju08@gmail.com>
2026-09-21 18:22:48 +0530

    Time: 358 ms (63.16%) | Memory: 34.2 MB (69.74%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def resultArray(self, nums, k):
+        ans = [0] * k
+        dp = [0] * k
+
+        for x in nums:
+            new = [0] * k
+            new[x % k] += 1
+
+            for r in range(k):
+                new[(r * x) % k] += dp[r]
+
+            dp = new
+
+            for r in range(k):
+                ans[r] += dp[r]
+
+        return ans
